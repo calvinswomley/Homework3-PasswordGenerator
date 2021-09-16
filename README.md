4b5f438075d1bec2c1110b23306403cd260caf85
Calvin Swomley <calvinswomley@gmail.com>
2021-09-15 22:41:40 -0600

    Updated readme, cleaned up comments and added icon

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Homework3-PasswordGenerator
 Week 3 JavaScript: Homework 3: Password Generator
 
-Description:
+Description: This web application randomly generates passwords for users. Users are prompted with password criteria questions to fit their particular needs. The Javascript code takes the user criteria inputs and randomly generates a password that meet their specified criteria. This password generator helps users create more secure passwords by random generation with a variety of possible character types and password lengths. The JavaScript code utilizes for loops, if/else statements, functions and event listeners.
 
 Completed Refactor Screenshot:
-![ Completed Password Generator refactor screenshot.](./assets/images/Completed_Homework_Mock-Up_Screenshot.PNG)
+![ Completed Password Generator refactor screenshot.](./assets/Finished_Site_Screenshot.JPG)
 
 
-Link to Deployed Application:
+Link to Deployed Application: https://calvinswomley.github.io/Homework3-PasswordGenerator/
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Generate buttom query selector
 var generateBtn = document.querySelector("#generate");
 
 //Set criteria variables
@@ -9,47 +9,37 @@ var lowercaseOption;
 var uppercaseOption;
 var passwordText = ""
 
-// Write password to the #password input
-// Password length prompt
+//Write password fuction
 function writePassword() {
-
+  //Password length prompt
   var lengthPrompt = window.prompt("How many characters?", "Type number between 8 and 128 here...");
   var lengthOption = lengthPrompt
   if (lengthOption <= 128 && lengthOption >= 8) {
-    console.log("lengthOption: " + lengthOption);
   } else if (!lengthPrompt) {
     return;
-    }
-    else {
-      console.log("Must be numeric whole number between 8 and 128");
+  } else {
       window.confirm("Must be numeric whole number between 8 and 128.")
       writePassword()
-    };
+  };
   
   //Special characters option prompt
   var specialPrompt = window.prompt("Include special characters? (Y for yes, N for no.)", "Type Y or N here...");
   var specialcharacterOption = specialPrompt
   if (specialcharacterOption === "Y" || specialcharacterOption === "N") {
-    console.log("specialcharacterOption: " + specialcharacterOption);
   } else if (!specialPrompt) {
       return;
   } else {
-    console.log("Value must be Y or N.");
     window.confirm("Value must be Y or N.")
     writePassword()
   };
 
-
-  // Numeric characters prompt
+  //Numeric characters prompt
   var numberPrompt = window.prompt("Include numeric characters? (Y for yes, N for no.)", "Type Y or N here...")
   var numberOption = numberPrompt
   if (numberOption === "Y" || numberOption === "N") {
-      console.log("numberOption: " + numberOption)
   } else if (!numberPrompt) {
     return;
-  }
-  else {
-    console.log("Value must be Y or N.");
+  } else {
     window.confirm("Value must be Y or N.")
     writePassword()
   };
@@ -59,12 +49,9 @@ function writePassword() {
   var uppercasePrompt = window.prompt("Include uppercase characters? (Y for yes, N for no.)", "Type Y or N here...")
   var uppercaseOption = uppercasePrompt
   if (uppercaseOption === "Y" || uppercaseOption === "N") {
-      console.log("uppercaseOption: " + uppercaseOption)
   } else if (!uppercasePrompt) {
     return;
-  }
-  else {
-    console.log("Value must be Y or N.");
+  } else {
     window.confirm("Value must be Y or N.")
     writePassword()
   };
@@ -73,32 +60,27 @@ function writePassword() {
   var lowercasePrompt = window.prompt("Include lowercase characters? (Y for yes, N for no.)", "Type Y or N here...")
   var lowercaseOption = lowercasePrompt
   if (lowercaseOption === "Y" || lowercaseOption === "N") {
-      console.log(lowercasePrompt)
   } else if (!lowercasePrompt) {
     return;
-  }
-  else {
-    console.log("Value must be Y or N.");
+  } else {
     window.confirm("Value must be Y or N.")
     writePassword()
   };
 
-  // Algorithm to take infomation from prompts and generate password
-
-  console.log("Password criteria collected: " + " lengthOption: " + lengthOption + " specialcharacterOption: " + specialcharacterOption + " numberOption: " + numberOption + " uppercaseOption: " + uppercaseOption + " lowercaseOption: " + lowercaseOption)
+  //Converting string to integer
   var lengthOption2 = parseInt(lengthOption, 10);
-  console.log(lengthOption2)
-  
-  //function generatePassword() {
-    for (; !(passwordText.length === lengthOption2); ) {
+
+  //Algorithm to take infomation from prompts and generate password.
+  //Random character type generator or loop continues until password length criteria is met
+  for (; !(passwordText.length === lengthOption2); ) {
       var optionSelector = getRandomIntInclusive();
       getRandomIntInclusive();
-        function getRandomIntInclusive(min, max) {
-            min = Math.ceil(5);
-            max = Math.floor(0);
-            return Math.floor(Math.random() * (max - min + 1) + min);
-            }
-        console.log(optionSelector)
+      function getRandomIntInclusive(min, max) {
+        min = Math.ceil(5);
+        max = Math.floor(0);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+      //Random number selector
       if (optionSelector == 1) {
         if (numberOption === "N") {
           continue;
@@ -106,71 +88,60 @@ function writePassword() {
         var numberSelector = getRandomIntInclusive();
         getRandomIntInclusive();
         function getRandomIntInclusive(min, max) {
-            min = Math.ceil(0);
-            max = Math.floor(9);
-            return Math.floor(Math.random() * (max - min + 1) + min);
-            }
-            passwordText += "" + numberSelector;
-            console.log(passwordText)
-            continue;
-      } else if (optionSelector == 2) {
+          min = Math.ceil(0);
+          max = Math.floor(9);
+          return Math.floor(Math.random() * (max - min + 1) + min);
+          }
+          passwordText += "" + numberSelector;
+          continue;
+      }
+      //Random special character selector
+      else if (optionSelector == 2) {
         if (specialcharacterOption === "N") {
           continue;
         }
         var speccharSelector =  randomSpecChar();
         randomSpecChar();
-          function randomSpecChar () {
-            var speciallist = "!#$%&*+<>?@^}{[]~";
-            return speciallist.substr(Math.floor(speciallist.length*Math.random()), 1);
-            }
-            passwordText += "" + speccharSelector;
-            console.log(passwordText)
-            continue;
-      } else if (optionSelector == 3) {
+        function randomSpecChar () {
+          var speciallist = "!#$%&*+<>?@^}{[]~";
+          return speciallist.substr(Math.floor(speciallist.length*Math.random()), 1);
+          }
+          passwordText += "" + speccharSelector;
+          continue;
+      }
+      //Random upper case letter selector
+      else if (optionSelector == 3) {
         if (uppercaseOption === "N") {
           continue;
         }
         var uppercaseSelector = randomUpperCaseLetter();
         randomUpperCaseLetter();
-          function randomUpperCaseLetter () {
-            var upperlist = "QWERTYUIOPASDFGHJKLZXCVBNM";
-            return upperlist.substr(Math.floor(upperlist.length*Math.random()), 1);
-            }
-            passwordText += "" + uppercaseSelector;
-            console.log(passwordText)
-            continue;
-          } else if (optionSelector == 4) {
-            if (lowercaseOption === "N") {
-              continue;
-            }
-            var lowercaseSelector = randomLowerCaseLetter();
-            randomLowerCaseLetter();
-              function randomLowerCaseLetter () {
-                var lowerlist = "qwertyuiopasdfghjklzxcvbnm";
-                return lowerlist.substr(Math.floor(lowerlist.length*Math.random()), 1);
-                }
-                passwordText += "" + lowercaseSelector;
-                console.log(passwordText)
-            continue;
-            } if (!((optionSelector == 1) || (optionSelector == 2) || (optionSelector == 3) || (optionSelector == 4 ))) {
-              console.log("error")
-            }
-            console.log(passwordText)
-            console.log("DOne!!!!!!")
-            document.getElementById("password").innerHTML = passwordText;
-
-    //var passwordText = document.querySelector("#password");
-  
-    //var password = generatePassword();
-
-  }
-  console.log("Password has been generated.")
+        function randomUpperCaseLetter () {
+          var upperlist = "QWERTYUIOPASDFGHJKLZXCVBNM";
+          return upperlist.substr(Math.floor(upperlist.length*Math.random()), 1);
+        }
+          passwordText += "" + uppercaseSelector;
+          continue;
+      }
+      //Random lower case letter selector
+      else if (optionSelector == 4) {
+        if (lowercaseOption === "N") {
+          continue;
+        }
+        var lowercaseSelector = randomLowerCaseLetter();
+        randomLowerCaseLetter();
+        function randomLowerCaseLetter () {
+          var lowerlist = "qwertyuiopasdfghjklzxcvbnm";
+          return lowerlist.substr(Math.floor(lowerlist.length*Math.random()), 1);
+          }
+          passwordText += "" + lowercaseSelector;
+          continue;
+      }
+    }
+  //Displays password in HTML viewable to the user
   document.getElementById("password").innerHTML = passwordText;
 };
 
-//don't forget 		b. VAR.capitalize() VAR.lowercase()
-
-
-// Add event listener to generate button (when button is clicked writePassword function executes)
+//Event listener that triggers writePassword function upon click of the generate password button
 generateBtn.addEventListener("click", writePassword);
 
